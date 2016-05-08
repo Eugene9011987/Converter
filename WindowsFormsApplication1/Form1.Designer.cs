4d8a5151380bd9d61a80c62d19b05096982fa83e
Eugene9011987 <eugene.levakhin@gmail.com>
2016-05-08 10:49:06 +0300

    converter

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.txtUSD = new System.Windows.Forms.TextBox();
+            this.txtEUR = new System.Windows.Forms.TextBox();
+            this.txtRUB = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 22);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(42, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "грн";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 44);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(42, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "руб";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 67);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(50, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "dollar";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(6, 90);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(46, 17);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "euro";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(70, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(70, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(70, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(70, 92);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 113);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(112, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(124, 111);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Конвертировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(111, 22);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 65);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "записать курс  по дате";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.richTextBox1.Location = new System.Drawing.Point(252, 185);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.richTextBox1.Size = new System.Drawing.Size(227, 80);
+            this.richTextBox1.TabIndex = 12;
+            this.richTextBox1.Text = "";
+            // 
+            // txtUSD
+            // 
+            this.txtUSD.Location = new System.Drawing.Point(14, 19);
+            this.txtUSD.Name = "txtUSD";
+            this.txtUSD.Size = new System.Drawing.Size(55, 20);
+            this.txtUSD.TabIndex = 14;
+            // 
+            // txtEUR
+            // 
+            this.txtEUR.Location = new System.Drawing.Point(14, 45);
+            this.txtEUR.Name = "txtEUR";
+            this.txtEUR.Size = new System.Drawing.Size(55, 20);
+            this.txtEUR.TabIndex = 15;
+            // 
+            // txtRUB
+            // 
+            this.txtRUB.Location = new System.Drawing.Point(14, 71);
+            this.txtRUB.Name = "txtRUB";
+            this.txtRUB.Size = new System.Drawing.Size(55, 20);
+            this.txtRUB.TabIndex = 16;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(71, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "USD";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(71, 48);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(30, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "EUR";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(71, 74);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(30, 13);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "RUB";
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(252, 12);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 20;
+            this.monthCalendar1.DateSelected += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateSelected);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtUSD);
+            this.groupBox1.Controls.Add(this.txtEUR);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.txtRUB);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(228, 100);
+            this.groupBox1.TabIndex = 21;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Курс валют к гривне:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton1);
+            this.groupBox2.Controls.Add(this.radioButton2);
+            this.groupBox2.Controls.Add(this.radioButton3);
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(12, 118);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(228, 150);
+            this.groupBox2.TabIndex = 22;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Конвертер";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(484, 277);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.richTextBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Конвертер валюты";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox txtUSD;
+        private System.Windows.Forms.TextBox txtEUR;
+        private System.Windows.Forms.TextBox txtRUB;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.XPath;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private double curseOfDollar;
+        private double curseOfEuro;
+        private double curseOfRubl;
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = "1";
+
+            string s = "";// строка с записью по последней дате = monthCalendar1.SelectionEnd.Day.ToString() + "." + monthCalendar1.SelectionEnd.Month.ToString() + "." + monthCalendar1.SelectionEnd.Year.ToString();
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load("Converter.xml");
+            DateTime maxDate = DateTime.MinValue;
+
+            //определение последней даты с записью курсов
+            foreach (XmlNode task in xmlDoc.DocumentElement.ChildNodes)
+            {
+                DateTime varDate = DateTime.Parse(task.Attributes.GetNamedItem("dt").Value);
+                monthCalendar1.AddBoldedDate(varDate);
+                if (varDate >= maxDate)
+                {
+                    maxDate = varDate;
+                    s = maxDate.Day.ToString() + "." + maxDate.Month.ToString() + "." + maxDate.Year.ToString();
+                }
+            }
+
+            // запись в поля курсов по последней дате записи
+            foreach (XmlNode task in xmlDoc.DocumentElement.ChildNodes)
+            {
+                if (task.Attributes.GetNamedItem("dt").Value == s)
+                {
+                    foreach (XmlNode param in task.ChildNodes)
+                    {
+                        if (param.Name == "usd")
+                        {
+                            s+= Environment.NewLine + "USD: " + param.InnerText;
+                            txtUSD.Text = param.InnerText;
+                        }
+                        if (param.Name == "eur")
+                        {
+                            s += Environment.NewLine + "EUR: " + param.InnerText;
+                            txtEUR.Text = param.InnerText;
+                        }
+                        if (param.Name == "rub")
+                        {
+                            s += Environment.NewLine + "RUB: " + param.InnerText;
+                            txtRUB.Text = param.InnerText;
+                        }
+
+                    }
+                } 
+            }
+            richTextBox1.Text = s;
+            radioButton3.Checked = true;
+        }
+
+        // функции конвертирования и вывода в метки результатов
+        private void grn()
+        {
+            if (!textBox1.Text.Equals(""))
+            {
+                label1.Text = "";
+                label2.Text = Convert.ToString(Convert.ToDouble(textBox1.Text) / curseOfRubl);
+                label3.Text = Convert.ToString(Convert.ToDouble(textBox1.Text) / curseOfDollar);
+                label4.Text = Convert.ToString(Convert.ToDouble(textBox1.Text) / curseOfEuro);
+            }
+        }
+        private void rub()
+        {
+            if (!textBox1.Text.Equals(""))
+            {
+                label2.Text = "";
+                label1.Text = Convert.ToString(Convert.ToDouble(textBox1.Text) * curseOfRubl);
+                label3.Text = Convert.ToString(Convert.ToDouble(textBox1.Text) * curseOfRubl / curseOfDollar);
+                label4.Text = Convert.ToString(Convert.ToDouble(textBox1.Text) * curseOfRubl / curseOfEuro);
+            }
+        }
+        private void dollar()
+        {
+            if (!textBox1.Text.Equals(""))
+            {
+                label3.Text = "";
+                label1.Text = Convert.ToString(Convert.ToDouble(textBox1.Text) * curseOfDollar);
+                label2.Text = Convert.ToString(Convert.ToDouble(textBox1.Text) * curseOfDollar / curseOfRubl);
+                label4.Text = Convert.ToString(Convert.ToDouble(textBox1.Text) * curseOfDollar / curseOfEuro);
+            }
+        }
+        private void euro()
+        {
+            if (!textBox1.Text.Equals(""))
+            {
+                label4.Text = "";
+                label1.Text = Convert.ToString(Convert.ToDouble(textBox1.Text) * curseOfEuro);
+                label2.Text = Convert.ToString(Convert.ToDouble(textBox1.Text) * curseOfEuro / curseOfRubl);
+                label3.Text = Convert.ToString(Convert.ToDouble(textBox1.Text) * curseOfEuro / curseOfDollar);
+            }
+        }
+
+        // кнопка конвертировать
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                double a = Convert.ToDouble(textBox1.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Некорректный ввод");
+                textBox1.Text = "";
+                return;
+            }
+
+            double us;
+            double eu;
+            double ru;
+
+            try
+            {
+                us = Convert.ToDouble(txtUSD.Text.Replace(',', '.'));
+                eu = Convert.ToDouble(txtEUR.Text.Replace(',', '.'));
+                ru = Convert.ToDouble(txtRUB.Text.Replace(',', '.'));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Некорректный ввод в одном из полей курсов валют");
+                return;
+            }
+
+            curseOfDollar = us;
+            curseOfEuro = eu;
+            curseOfRubl = ru;
+
+            if (!textBox1.Text.Equals(""))
+            {
+                if (radioButton1.Checked)
+                {
+                    grn();
+                }
+                if (radioButton2.Checked)
+                {
+                    rub();
+                }
+                if (radioButton3.Checked)
+                {
+                    dollar();
+                }
+                if (radioButton4.Checked)
+                {
+                    euro();
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // создание  XML файла
+            ////<?xml version = "1.0" encoding = "iso-8859-1"?>
+            //XmlTextWriter textWritter = new XmlTextWriter("Converter.xml", Encoding.UTF8);
+            //textWritter.Formatting = Formatting.Indented;
+            //textWritter.IndentChar = '\t';
+            //textWritter.Indentation = 1;
+
+            //textWritter.WriteStartDocument();
+            //textWritter.WriteStartElement("converter");
+            //textWritter.WriteStartElement("date");
+
+            //textWritter.WriteStartAttribute("dt");
+            //textWritter.WriteString(DateTime.Now.Day.ToString() + "." + DateTime.Now.Month.ToString() + "." + DateTime.Now.Year.ToString());
+            //textWritter.WriteEndAttribute();
+
+            //textWritter.WriteStartElement("usd");
+            //textWritter.WriteString("21.56");
+            //textWritter.WriteEndElement();
+
+            //textWritter.WriteStartElement("eur");
+            //textWritter.WriteString("23.34");
+            //textWritter.WriteEndElement();
+
+            //textWritter.WriteStartElement("rub");
+            //textWritter.WriteString("0.37");
+            //textWritter.WriteEndElement();
+
+            //textWritter.WriteEndElement();
+            //textWritter.WriteEndElement();
+            //textWritter.Close();
+
+            string s = monthCalendar1.SelectionEnd.Day.ToString() + "." + monthCalendar1.SelectionEnd.Month.ToString() + "." + monthCalendar1.SelectionEnd.Year.ToString();
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load("Converter.xml");
+
+            // чтение XML файла
+
+            // удаление записи, с одинаковой датой
+            foreach (XmlNode task in xmlDoc.DocumentElement.ChildNodes)
+            {
+                if (task.Attributes.GetNamedItem("dt").Value == s)
+                {
+                    var navigator = task.CreateNavigator();
+                    navigator.DeleteSelf();
+                    //task.RemoveAll();
+                    xmlDoc.Save("Converter.xml");
+                    break;
+                }
+            }
+
+            //  запись в XML файд
+            XmlDocument document = new XmlDocument();
+            document.Load("Converter.xml");
+
+            XmlNode element = document.CreateElement("date");
+            document.DocumentElement.AppendChild(element); // указываем родителя
+            XmlAttribute attribute = document.CreateAttribute("dt"); // создаём атрибут
+            attribute.Value = monthCalendar1.SelectionEnd.Day.ToString() + "." + monthCalendar1.SelectionEnd.Month.ToString() + "." + monthCalendar1.SelectionEnd.Year.ToString(); // устанавливаем значение атрибута
+            element.Attributes.Append(attribute); // добавляем атрибут
+
+            XmlNode subElement1 = document.CreateElement("usd"); // даём имя
+            subElement1.InnerText = txtUSD.Text; // и значение
+            element.AppendChild(subElement1); // и указываем кому принадлежит
+
+            XmlNode subElement2 = document.CreateElement("eur"); // даём имя
+            subElement2.InnerText = txtEUR.Text; // и значение
+            element.AppendChild(subElement2); // и указываем кому принадлежит
+
+            XmlNode subElement3 = document.CreateElement("rub"); // даём имя
+            subElement3.InnerText = txtRUB.Text; // и значение
+            element.AppendChild(subElement3); // и указываем кому принадлежит
+
+            document.Save("Converter.xml");
+            monthCalendar1.AddBoldedDate(DateTime.Parse(s));
+            monthCalendar1.UpdateBoldedDates();
+            monthCalendar1_DateSelected(null, null);
+        }
+
+        private void monthCalendar1_DateSelected(object sender, DateRangeEventArgs e)
+        {
+            string s = monthCalendar1.SelectionEnd.Day.ToString() + "." + monthCalendar1.SelectionEnd.Month.ToString() + "." + monthCalendar1.SelectionEnd.Year.ToString();
+            
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load("Converter.xml");
+            string buff = "";                        // строка для вывода информации в текстовое поле
+
+            // чтение XML файла
+            foreach (XmlNode task in xmlDoc.DocumentElement.ChildNodes)
+            {
+               
+                if (task.Attributes.GetNamedItem("dt").Value == s)
+                {
+                    foreach (XmlNode param in task.ChildNodes)
+                    {
+                        if (param.Name == "usd")
+                        {
+                            buff += Environment.NewLine + "USD: " + param.InnerText;
+                            txtUSD.Text = param.InnerText;
+                        }
+                        if (param.Name == "eur")
+                        {
+                            buff += Environment.NewLine + "EUR: " + param.InnerText;
+                            txtEUR.Text = param.InnerText;
+                        }
+                        if (param.Name == "rub")
+                        {
+                            buff += Environment.NewLine + "RUB: " + param.InnerText;
+                            txtRUB.Text = param.InnerText;
+                        }
+
+                    }
+                }
+            }
+            richTextBox1.Text = s + buff;
+            button1_Click(null, null);
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            button1_Click(null, null);
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            button1_Click(null, null);
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            button1_Click(null, null);
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            button1_Click(null, null);
+        }
+    } 
+} 
